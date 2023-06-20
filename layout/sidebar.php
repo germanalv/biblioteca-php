f2920e98f7f6c7b3923d576c7e69fc2856a1e4af
German Alvarez <germanalv@gmail.com>
2023-06-20 14:40:35 -0300

    2do Commit

---
+++ layout/sidebar.php
@@ -0,0 +1,22 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+          <span class="fs-4">Bliblioteca</span>
+      </a>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link active" aria-current="page">
+              Prestamos
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link text-white">
+              Libros
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link text-white">
+            Usuarios
+          </a>
+        </li>
+      </ul>
---
+++ vistas/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include '../layout/head.php';?>    
+</head>
+
+<body>
+  <main class="d-flex flex-nowrap">
+    <!-- Manu -->
+    <?php include '../layout/sidebar.php';?>
+    
+      <hr>
+
+      <!-- Menu de Pefil de usuario -->
+      <?php include '../layout/perfil.php';?>
+
+    </div>
+
+    <div class="d-flex flex-column p-3 w-100">
+        <div class="container">
+            <!-- Contenedor Principal -->
+
+
+            <h2 class="text-center">main</h2>
+        </div>
+    </div>
+
+
+  </main>
+
+
+  <!-- Menu de Pefil de usuario -->
+  <?php include '../layout/footer.php';?>
+</body>
+</html>
